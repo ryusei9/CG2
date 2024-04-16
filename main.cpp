a6419499749a696b86ae33904493ec98fd8f5cc2
ryusei9 <k023g0066@g.neec.ac.jp>
2024-04-17 00:42:10 +0900

    DirectX12の初期化が完了した

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <cstdint>
 #include <string>
 #include <format>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
 
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 // ウィンドウプロシーシャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	WPARAM wparam, LPARAM lparam) {
@@ -117,6 +122,56 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// 変数から型を推論してくれる
 	Log(std::format("str0:{},str1:{}\n", str0,str1));
+
+	// DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+	// 関数が成功したかどうか
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	// エラー識別
+	assert(SUCCEEDED(hr));
+	// 使用するアダプタ用の変数
+	IDXGIAdapter4* useAdapter = nullptr;
+	// 良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+		// アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		// ソフトウェアアダプタでなければ使用
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			// 採用したアダプタの情報をログに出力
+			// その際std::wstringをstd::stringに変換
+			Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		// ソフトウェアアダプタは無視
+		useAdapter = nullptr;
+	}
+	// 適切なアダプタが無いときは起動できない
+	assert(useAdapter != nullptr);
+
+	ID3D12Device* device = nullptr;
+	// 機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	}; const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	// 高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); ++i) {
+		// 採用したアダプターでデバイスを生成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		// 指定した機能レベルでデバイスが生成できたかを確認
+		if (SUCCEEDED(hr)) {
+			// 生成できたのでログ出力をおっこなってループを抜ける
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	// デバイスの生成が上手くいかなかったので起動できない
+	assert(device != nullptr);
+	// 初期化完了のログを出す
+	Log("Complete create D3D12Device!!!\n");
 
 	MSG msg{};
 	// ウィンドウの×ボタンが押されるまでループ
