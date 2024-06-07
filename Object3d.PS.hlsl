be7e7dfe61af2e7ddaa18dd50c34e395b320384f
ryusei9 <k023g0066@g.neec.ac.jp>
2024-06-07 14:53:22 +0900

    Half Lambertができた

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -33,9 +33,13 @@ PixelShaderOutput main(VertexShaderOutput input){
     output.color = gMaterial.color;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     //output.color = gMaterial.color * textureColor;
+    
+    
     if (gMaterial.enableLighting != 0)
     { // Lightingする場合
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        // half lambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
     else
